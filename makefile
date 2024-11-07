ac2f3f72a417944071b710e6f5186a5cdbde4671
Harriet Soulsby <hs1956@york.ac.uk>
2024-11-07 11:28:00 +0000

    Parallelized the code using openMP. Also set it up to import parameters from a file, rather than needing to recompile when changing the properties of the laser.

--- makefile
+++ makefile
@@ -3,9 +3,9 @@ CC=g++
 # Libraries
 LIBS=-lstdc++ -lgsl -lgslcblas
 # Compilation flags
-CC_CFLAGS=-O3 -mtune=native -I./hdr -std=c++20
+CC_CFLAGS=-O3 -mtune=native -I./hdr -std=c++20 -fopenmp
 # Link flags
-CC_LDFLAGS= -lstdc++ -I./hdr
+CC_LDFLAGS= -lstdc++ -I./hdr -std=c++20 -fopenmp
 # Object (source) files
 OBJECTS= obj/main.o obj/calculate_satellite_distance.o obj/calculate_height.o obj/integrate_turbulence.o obj/calculate_beam_spread.o obj/calculate_beam_intensity.o
 # Name of generated executable
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <numbers>
 #include <fstream>
 #include <iomanip>
+#include <omp.h>
 
 // Defines main
 int main(){
@@ -14,29 +15,45 @@ int main(){
 	double integrated_turbulence;
 	int satellite_distance;
 
+	// Initialises the variables to store the user defined parameters for the system
+	int satellite_altitude;
+	float diameter_laser, wavelength_laser, inner_scale_size;
+
+	// Sets up a stream for importing parameters from an input file
+	std::ifstream parameterFile;
+	parameterFile.open("input_parameters");
+
+	//Imports the parameters from the file
+	std::string dummy;
+	std::getline(parameterFile, dummy);
+	parameterFile >> dummy >> diameter_laser;
+	parameterFile >> dummy >> wavelength_laser;
+	parameterFile >> dummy >> inner_scale_size;
+	parameterFile >> dummy >> satellite_altitude;
+	parameterFile.close();
+
 	// Defines our constants for the system
 	int radius_earth = 6371000;
-	int radius_LEO = radius_earth + 800000;
+	int radius_LEO = radius_earth + satellite_altitude;
 
-	// Defines the parameters for the laser and atmosphere
-	float diameter_laser = 0.1;
-	float wavelength_laser = 1550e-9;
+	// Calculates the parameters for the laser
 	float wavenumber_laser = (2 * std::numbers::pi) / wavelength_laser;
 	float curvature_laser = diameter_laser / wavelength_laser;
-	float inner_scale_size = 0.005;
 
-	// Defines the angle parameters we will iterate over
+	// Defines the angle parameters we will er
 	double angle, angle_degrees;
 
 	// Sets up a data file to write our outputs into
-	std::ofstream dataFile("data");
+	std::ofstream dataFile;
+	dataFile.open("output_data");
 	dataFile << std::fixed << std::setprecision(20);
 
 	// Loops through angles more than 30 degrees above the horizon, performing the turbulence simulation each time
-	for(double i = 30; i <= 150; i+=0.01){
+	#pragma omp parallel for private(angle, angle_degrees, beam_spread, beam_intensity, integrated_turbulence, satellite_distance)
+	for(int i = 3000; i <= 15000; i+=1){
 
 		// Converts the angle to radians
-		angle_degrees = i;
+		angle_degrees = i / 100.0;
 		angle = angle_degrees * std::numbers::pi / 180.0;
 
 		// Calculate the effects of turbulence on the laser
@@ -46,7 +63,10 @@ int main(){
 		beam_intensity = calculate_beam_intensity(satellite_distance, integrated_turbulence, diameter_laser, wavenumber_laser, curvature_laser, inner_scale_size);
 
 		// Outputs the calculated parameters to the datafile, along with the current angle in degrees
-		dataFile << angle_degrees << " " << beam_spread << " " << beam_intensity << std::endl;
+		#pragma omp critical
+		{
+			dataFile << angle_degrees << " " << beam_spread << " " << beam_intensity << std::endl;
+		}
 
 	}
 
